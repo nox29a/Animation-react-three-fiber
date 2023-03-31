b73082e292596f07d3106c9c71d2155772bbb22f
nox29a <adamczyk29a@interia.pl>
2023-03-31 13:30:34 +0200

    grid added fix bugs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import * as THREE from 'three'
-import { Suspense, useRef, useState, useMemo, useEffect } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef} from 'react'
+import { Canvas} from '@react-three/fiber'
 import { Physics, RigidBody } from '@react-three/rapier'
-
-const niceColors = ['blue', 'brown', 'green', 'orange', 'purple', 'red']
+import { OrbitControls} from '@react-three/drei'
+const Colors = ['blue', 'pink', 'green', 'yellow', 'purple', 'red']
 export default function App() {
-  const balls = Array.from({ length: 20 }, () => ({
+  const balls = Array.from({ length: 25 }, () => ({
     position: [
-      (Math.random() - 0.5) * 2, // x coordinate between -10 and 10
-      Math.random() * 1, // y coordinate between 5 and 15
-      (Math.random() - 0.5) * 2 // z coordinate between -10 and 10
+      Math.floor(Math.random() * 10)-5,
+      Math.floor(Math.random() * 10)-5,
+      5
     ]
   }));
   return (
-    <Canvas style={{ background: "black", position: "absolute", top: 0, left: 0 }} camera={{ position: [-50, -25, 150], fov: 15 }}>
-      <Suspense fallback={null}>
-        <hemisphereLight intensity={0.9} />
-        <ambientLight intensity={0.9} />
-        <Physics colliders={false}>
-        {balls.map((ball, index) => (
-          <Sphere key={index} position={ball.position} />
-        ))}
+    <Canvas style={{ background: "black", position: "absolute", top: 0, left: 0 }} camera={{ position: [0, 0, 150], fov: 30 }}>
+      <pointLight position={[0, 0, 50]} intensity={1}/>
+        <Physics>
+          {balls.map((ball, index) => (
+            <Sphere key={index} position={[0,0,20-index]} />
+          ))}
           <Container />
+          <Frame />
         </Physics>
-      </Suspense>
+        <gridHelper args={[120, 40, "teal", "gray"]} rotation-x={Math.PI / 2} />
+        <OrbitControls />
     </Canvas>
   )
 }
@@ -31,22 +30,29 @@ export default function App() {
 function Container() {
   const ref = useRef()
   return (
-    <group>
       <RigidBody ref={ref} type="fixed" colliders="trimesh" restitution={1}>
-        <mesh visible="false" rotation={[Math.PI / 2, 0, 0]}>
-          <sphereGeometry args={[15, 20, 20]} />
-          <meshStandardMaterial color="orange"  opacity={0.1} transparent  />
+        <mesh visible="false" rotation={[0, 0, 0]}>
+          <sphereGeometry args={[30, 700, 700]} />
+          <meshStandardMaterial color="black"  opacity={0.1} transparent  />
         </mesh>
       </RigidBody>
-    </group>
+  )
+}
+function Frame() {
+  const ref = useRef()
+  return (
+        <mesh visible="false" >
+          <ringGeometry args={[30, 30.5, 100]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
   )
 }
 
 const Sphere = (props) => (
   <RigidBody colliders="ball" restitution={1}>
-    <mesh castShadow receiveShadow {...props}>
-      <sphereGeometry args={[0.7, 10, 10]} />
-      <meshStandardMaterial color={niceColors[Math.floor(Math.random() * niceColors.length)]} />
+    <mesh {...props}>
+      <sphereGeometry args={[1, 100, 100]} />
+      <meshStandardMaterial color={Colors[Math.floor(Math.random() * Colors.length)]} />
     </mesh>
   </RigidBody>
 )
